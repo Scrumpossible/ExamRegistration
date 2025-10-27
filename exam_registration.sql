bebc144093158f7e7fa00930fca33b6a0dd27df6
I75zt4UBd39ln17E <itzabellestudent@gmail.com>
2025-10-27 15:47:12 -0700

    Added 'ON DELETE' to avoid orphaned data

--- exam_registration.sql
+++ exam_registration.sql
@@ -1,10 +1,9 @@
-
 CREATE TABLE exam_registration.users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     nshe_num VARCHAR(10) UNIQUE NOT NULL,
     full_name VARCHAR(255) NOT NULL,
-    role ENUM('student', 'faculty') NOT NULL,
+    role ENUM('student', 'faculty', 'admin') NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
@@ -32,23 +31,24 @@ CREATE TABLE exam_registration.exam_proctors (
 
 CREATE TABLE exam_registration.exam_sessions (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    exam_id INT NOT NULL,
     location_id INT NOT NULL,
     proctor_id INT NULL,
     session_date DATE NOT NULL,
     session_time TIME NOT NULL,
-    capacity INT DEFAULT 20,
-    FOREIGN KEY (exam_id) REFERENCES exams(id),
-    FOREIGN KEY (location_id) REFERENCES locations(id),
-    FOREIGN KEY (proctor_id) REFERENCES users(id)
+    capacity INT NOT NULL DEFAULT 20,
+    FOREIGN KEY (location_id) REFERENCES locations(id) ON DELETE CASCADE,
+    FOREIGN KEY (proctor_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
 CREATE TABLE exam_registration.registrations (
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     session_id INT NOT NULL,
+    exam_id INT NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (session_id) REFERENCES exam_sessions(id),
-    UNIQUE (user_id, session_id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (session_id) REFERENCES exam_sessions(id) ON DELETE CASCADE,
+    FOREIGN KEY (exam_id) REFERENCES exams(id) ON DELETE CASCADE,
+    UNIQUE (user_id, session_id),
+    UNIQUE (user_id, exam_id)
 );
